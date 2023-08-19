c20c2387d5d107de63aa9ceb177d995cd0ad9aad
김어진 <linexy@naver.com>
2023-08-19 20:27:45 +0900

    Initial commit

---
+++ app/src/main/java/com/ej/db/TextDatabase.kt
@@ -0,0 +1,35 @@
+package com.ej.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.ej.db.dao.TextDao
+import com.ej.db.entity.TextEntity
+
+@Database(entities = [TextEntity::class], version = 1)
+abstract class TextDatabase  : RoomDatabase(){
+    abstract fun textDao() :TextDao
+
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE : TextDatabase? = null
+
+        fun getDatabase(context: Context) : TextDatabase{
+
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TextDatabase::class.java,
+                    "text_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE  = instance
+                instance
+            }
+        }
+    }
+}
